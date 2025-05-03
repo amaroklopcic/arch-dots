15ead4c41aca5a189640c0607761c89b28d20c38
Amar Oklopcic <amaroklopcic@icloud.com>
2025-05-02 20:54:07 -0500

    add a more polished waybar add tofi config

--- dot_local/bin/executable_toggle-theme.sh
+++ dot_local/bin/executable_toggle-theme.sh
@@ -9,3 +9,8 @@ if [[ "$current" == "'prefer-dark'" ]]; then
 else
     $HOME/.local/bin/enable-theme-dark.sh
 fi
+
+# Have waybar reload our dark/light theme module to reflect new
+# dark mode state
+pkill -SIGRTMIN+10 waybar
+
